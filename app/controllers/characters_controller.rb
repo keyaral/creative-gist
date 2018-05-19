cb1dad2c6fd8e4809532a3b4041364470eb58a13
Marian Clements <marian@rabidtech.co.nz>
2018-05-19 18:40:27 +1200

    filling in the gaps

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -27,6 +27,8 @@ class CharactersController < ApplicationController
   end
 
   def update
+    @character = Character.find(:params[:id])
+
     if @character.update(character_params)
       redirect_to characters_path, notice: "Character was updated successfully"
     else
